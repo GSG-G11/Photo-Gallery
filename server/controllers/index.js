21c7b6593533346e40a8f9367c67eae68b89dc67
zaher-aa <zaherabuamro@gmail.com>
2022-02-22 17:56:54 +0200

    Added error handlers exports and imports to controllers and routes Relates #9

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,9 @@
 const handleHomePage = require('./handleHomePage');
+const pageNotFound = require('./pageNotFound');
+const serverError = require('./serverError');
 
 module.exports = {
   handleHomePage,
+  pageNotFound,
+  serverError,
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+
 const {
   handleHomePage,
+  pageNotFound,
+  serverError,
 } = require('../controllers');
 
 router.get('/', handleHomePage);
+router.use(pageNotFound);
+router.use(serverError);
 
 module.exports = router;
