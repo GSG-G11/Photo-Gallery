a9c37d19fe8c53f6bc378a1d40bca36196fa13a7
khaled <yonkoluffy48@gmail.com>
2022-02-22 18:15:02 +0200

    resolve endpoint about and status of error pages

--- server/controllers/serverError.js
+++ server/controllers/serverError.js
@@ -1,7 +1,7 @@
 const { join } = require('path');
 
 const serverError = (err, req, res, next) => {
-  res.sendFile(join(__dirname, '../../public/html/500.html'));
+  res.status(500).sendFile(join(__dirname, '../../public/html/500.html'));
 };
 
 module.exports = serverError;
