374fe8c64c5876b286bbf0ddb498a5130b540646
zaher-aa <zaherabuamro@gmail.com>
2022-02-22 21:21:53 +0200

    Added handleSearch imports and exports Relates #10

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,12 +3,14 @@ const router = require('express').Router();
 const {
   handleHomePage,
   pageNotFound,
+  handleSearch,
   serverError,
   handleAboutPage,
 } = require('../controllers');
 
 router.get('/', handleHomePage);
 router.get('/about', handleAboutPage);
+router.post('/search', handleSearch);
 router.use(pageNotFound);
 router.use(serverError);
 
