084ac77ea9676e5481be55a4d89cd0af66f1447d
zaher-aa <zaherabuamro@gmail.com>
2022-02-23 08:10:30 +0200

    Removed homepage handler Relates #30

--- server/controllers/handleHomePage.js
+++
@@ -1,7 +0,0 @@
-const { join } = require('path');
-
-const handleHomePage = (req, res) => {
-  res.sendFile(join(__dirname, '../../public/index.html'));
-};
-
-module.exports = handleHomePage;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,12 @@
 const router = require('express').Router();
 
 const {
-  handleHomePage,
   pageNotFound,
   handleSearch,
   serverError,
   handleAboutPage,
 } = require('../controllers');
 
-router.get('/', handleHomePage);
 router.get('/about', handleAboutPage);
 router.post('/search', handleSearch);
 router.use(pageNotFound);
