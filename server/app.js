ec48a252fda60f2744f9c7bf1d4a4e27ec97e0d9
khaled <yonkoluffy48@gmail.com>
2022-02-23 13:12:12 +0200

    edit port command

--- server/app.js
+++ server/app.js
@@ -20,6 +20,6 @@ app.use(express.static(join(__dirname, '../public'), { maxAge: '30d' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(router);
-app.set('port', process.env.PROT || 3000);
+app.set('port', process.env.PORT || 3000);
 
 module.exports = app;
