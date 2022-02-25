4ae2671fe6b7e50921a6addff70ca19e4bd70760
zaher-aa <zaherabuamro@gmail.com>
2022-02-25 15:09:59 +0200

    copy gif link when click, and give a default value of the limit input field Relates #47

--- public/js/dom.js
+++ public/js/dom.js
@@ -3,15 +3,15 @@ const form = document.forms[0];
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const input = form.searchTerm.value;
-  const imgsNum = form.limit.value;
+  const imgsNum = form.limit.value || 5;
 
-  if (input.trim() !== '' && isNaN(input) && (+imgsNum > 0 && +imgsNum <= 20)) {
-    toggleLoaderDisplay();
-    makeRequest({ searchTerm: input, limit: imgsNum }, '/search', 'POST')
+  if (input.trim() !== '' && isNaN(input) && (+imgsNum > 0 && +imgsNum <= 50)) {
+    toggleLoader();
+    makeRequest(`/search?q=${input}&limit=${imgsNum}`)
       .then((checkData))
       .catch(() => invalidInput(`Sorry, we don't have ${input}`));
   } else {
-    invalidInput('Search Term should be a word and images number should be in the range 1 >= image number <= 20');
+    invalidInput('Search Term should be a word and images number should be in the range 1 >= image number <= 50');
   }
 });
 
